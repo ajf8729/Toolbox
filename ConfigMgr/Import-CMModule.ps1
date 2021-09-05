edceb1f7eb789a7887591ba18424861d659d01f7
Anthony Fontanez <ajf@anthonyfontanez.com>
2021-09-04 22:19:03 -0400

    Removed try/catch as it's not necessary

--- ConfigMgr/Import-CMModule.ps1
+++ ConfigMgr/Import-CMModule.ps1
@@ -1,6 +1,1 @@
-try {
-    Import-Module -FullyQualifiedName "$env:SMS_ADMIN_UI_PATH\..\ConfigurationManagers.psd1"
-}
-catch {
-    Write-Warning -Message $_.Exception.Message
-}
+Import-Module -FullyQualifiedName "$env:SMS_ADMIN_UI_PATH\..\ConfigurationManagers.psd1"
