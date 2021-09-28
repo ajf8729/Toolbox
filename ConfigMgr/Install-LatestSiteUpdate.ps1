8c96e946f0dc3d2177bc073b8c5470d55d2b217b
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2021-09-28 02:18:39 -0400

    Corrected sort order

--- ConfigMgr/Install-LatestSiteUpdate.ps1
+++ ConfigMgr/Install-LatestSiteUpdate.ps1
@@ -5,6 +5,6 @@ $SiteCode = (Get-CimInstance -Namespace ROOT/SMS -ClassName SMS_ProviderLocation
 # Change to the site psdrive
 Set-Location -Path "$($SiteCode):"
 # Find the latest update
-$LatestSiteUpdate = (Get-CMSiteUpdate -Fast | Sort-Object -Property LastUpdateTime | Select-Object -First 1).Name
+$LatestSiteUpdate = (Get-CMSiteUpdate -Fast | Sort-Object -Descending -Property LastUpdateTime | Select-Object -First 1).Name
 # Install the update
 Install-CMSiteUpdate -Name $LatestSiteUpdate -Confirm:$false -Force
