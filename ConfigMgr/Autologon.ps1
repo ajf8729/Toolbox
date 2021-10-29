6820224f90b371d6609a49700aa0fec465ac88c6
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2021-10-29 08:51:43 -0400

    Updated variable names

--- ConfigMgr/Autologon.ps1
+++ ConfigMgr/Autologon.ps1
@@ -1,9 +1,9 @@
 $TSEnv = New-Object -COMObject Microsoft.SMS.TSEnvironment
 
-$Username = $TSEnv.Value("Username")
-$Domain   = $TSEnv.Value("Domain")
-$Password = $TSEnv.Value("Password")
+$AutologonUsername = $TSEnv.Value("AutologonUsername")
+$AutologonDomain   = $TSEnv.Value("AutologonDomain")
+$AutologonPassword = $TSEnv.Value("AutologonPassword")
 
 $ScriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-Start-Process -FilePath "$($ScriptDirectory)\Autologon64.exe" -ArgumentList "/AcceptEula $Username $Domain $Password"
+Start-Process -FilePath "$($ScriptDirectory)\Autologon64.exe" -ArgumentList "/AcceptEula $AutologonUsername $AutologonDomain $AutologonPassword"
