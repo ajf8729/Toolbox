ced5fa12cc578ce4e4e25d0543c73b4f40b997fe
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2021-10-29 08:03:41 -0400

    Updated New-ModelCollections.ps1

--- ConfigMgr/New-ModelCollections.ps1
+++ ConfigMgr/New-ModelCollections.ps1
@@ -1,39 +1,21 @@
-#Original credit to /u/houstonau on Reddit
-#https://www.reddit.com/r/SCCM/comments/5jhcpk/create_sccm_device_collections_based_on_model/
-#Modified to update collections daily instead of incrementally
+Import-Module -FullyQualifiedName "$env:SMS_ADMIN_UI_PATH\..\ConfigurationManager.psd1"
 
-[CmdletBinding()]
-Param (
-    [Parameter(Mandatory = $true)]
-    [string]$cmSiteID
-)
+$SiteCode = (Get-CimInstance -Namespace ROOT/SMS -ClassName SMS_ProviderLocation).SiteCode
 
-Import-Module ${env:ProgramFiles(x86)}\ConfigMgrConsole\bin\ConfigurationManager.psd1
+Set-Location -Path "$($SiteCode):"
 
-$rootCMDrive = "$($cmSiteID):"
-$curModels = @()
-$Schedule = New-CMSchedule –RecurInterval Days –RecurCount 1
+$Schedule = New-CMSchedule -RecurInterval Days -RecurCount 1
 
-If (!(Get-Item $rootCMDrive\DeviceCollection\Models))
-{
-    New-item $rootCMDrive\DeviceCollection\Models
+if (-not (Test-Path -Path "$($SiteCode):\DeviceCollection\Models")) {
+    New-Item -Path "$($SiteCode):\DeviceCollection\" -Name "Models"
 }
 
-function newModelCollection ([string]$modelName)
-{
-    Set-Location $rootCMDrive
-    if (!(Get-CMDeviceCollection -Name $modelName))
-    {
-        New-CMDeviceCollection -Name $modelName -LimitingCollectionName 'All Systems' -RefreshSchedule $Schedule -RefreshType 2
-        Add-CMDeviceCollectionQueryMembershipRule -CollectionName $modelName -QueryExpression "select SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.ResourceDomainORWorkgroup,SMS_R_SYSTEM.Client from SMS_R_System inner join SMS_G_System_COMPUTER_SYSTEM on SMS_G_System_COMPUTER_SYSTEM.ResourceId = SMS_R_System.ResourceId where SMS_G_System_COMPUTER_SYSTEM.Model = `"$($modelName)`"" -RuleName "Model is $($modelName)"
-        Move-CMObject -InputObject (Get-CMDeviceCollection -Name $modelName) -FolderPath "$($rootCMDrive)\DeviceCollection\Models"
+$Models = Invoke-CMWmiQuery -Query "select distinct SMS_G_System_COMPUTER_SYSTEM.Manufacturer, SMS_G_System_COMPUTER_SYSTEM.Model from  SMS_R_System inner join SMS_G_System_COMPUTER_SYSTEM on SMS_G_System_COMPUTER_SYSTEM.ResourceID = SMS_R_System.ResourceId" | Select-Object -ExpandProperty Model
+
+foreach ($Model in $Models) {
+    if (-not (Get-CMDeviceCollection -Name $Model)) {
+        $Collection = New-CMDeviceCollection -Name $Model -LimitingCollectionName 'All Systems' -RefreshSchedule $Schedule -RefreshType 2
+        Add-CMDeviceCollectionQueryMembershipRule -CollectionName $Model -QueryExpression "select SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.ResourceDomainORWorkgroup,SMS_R_SYSTEM.Client from SMS_R_System inner join SMS_G_System_COMPUTER_SYSTEM on SMS_G_System_COMPUTER_SYSTEM.ResourceId = SMS_R_System.ResourceId where SMS_G_System_COMPUTER_SYSTEM.Model = `"$($Model)`"" -RuleName $Model
+        Move-CMObject -InputObject $Collection -FolderPath "$($SiteCode):\DeviceCollection\Models"
     }
-}
-
-Set-Location $rootCMDrive
-$curModels = Invoke-CMWmiQuery -query "select distinct SMS_G_System_COMPUTER_SYSTEM.Manufacturer, SMS_G_System_COMPUTER_SYSTEM.Model from  SMS_R_System inner join SMS_G_System_COMPUTER_SYSTEM on SMS_G_System_COMPUTER_SYSTEM.ResourceID = SMS_R_System.ResourceId" | Select Manufacturer, Model
-
-foreach ($model in $curModels)
-{
-	newModelCollection($model.model)
 }
