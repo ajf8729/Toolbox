0a38a78a9a6e7f56cad5aadc782d61013c4d982d
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2021-12-02 13:46:40 -0500

    Fixed formatting

--- ConfigMgr/Get-MSIFileInformation.ps1
+++ ConfigMgr/Get-MSIFileInformation.ps1
@@ -2,12 +2,12 @@
 
 [CmdletBinding()]
 Param(
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
     [System.IO.FileInfo]$Path,
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
-    [ValidateSet(“ProductCode”, “ProductVersion”, “ProductName”, “Manufacturer”, “ProductLanguage”, “FullVersion”)]
+    [ValidateSet('ProductCode', 'ProductVersion', 'ProductName', 'Manufacturer', 'ProductLanguage', 'FullVersion')]
     [string]$Property
 )
 
@@ -15,16 +15,16 @@ process {
     try {
         # Read property from MSI database
         $WindowsInstaller = New-Object -ComObject WindowsInstaller.Installer
-        $MSIDatabase = $WindowsInstaller.GetType().InvokeMember("OpenDatabase", "InvokeMethod", $null, $WindowsInstaller, @($Path.FullName, 0))
+        $MSIDatabase = $WindowsInstaller.GetType().InvokeMember('OpenDatabase', 'InvokeMethod', $null, $WindowsInstaller, @($Path.FullName, 0))
         $Query = "SELECT Value FROM Property WHERE Property = '$($Property)'"
-        $View = $MSIDatabase.GetType().InvokeMember("OpenView", "InvokeMethod", $null, $MSIDatabase, ($Query))
-        $View.GetType().InvokeMember("Execute", "InvokeMethod", $null, $View, $null)
-        $Record = $View.GetType().InvokeMember("Fetch", "InvokeMethod", $null, $View, $null)
-        $Value = $Record.GetType().InvokeMember("StringData", "GetProperty", $null, $Record, 1)
+        $View = $MSIDatabase.GetType().InvokeMember('OpenView', 'InvokeMethod', $null, $MSIDatabase, ($Query))
+        $View.GetType().InvokeMember('Execute', 'InvokeMethod', $null, $View, $null)
+        $Record = $View.GetType().InvokeMember('Fetch', 'InvokeMethod', $null, $View, $null)
+        $Value = $Record.GetType().InvokeMember('StringData', 'GetProperty', $null, $Record, 1)
 
         # Commit database and close view
-        $MSIDatabase.GetType().InvokeMember("Commit", "InvokeMethod", $null, $MSIDatabase, $null)
-        $View.GetType().InvokeMember("Close", "InvokeMethod", $null, $View, $null)
+        $MSIDatabase.GetType().InvokeMember('Commit', 'InvokeMethod', $null, $MSIDatabase, $null)
+        $View.GetType().InvokeMember('Close', 'InvokeMethod', $null, $View, $null)
         $MSIDatabase = $null
         $View = $null
 
