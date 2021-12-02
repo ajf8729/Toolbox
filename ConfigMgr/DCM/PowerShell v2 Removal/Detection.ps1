5729ee52dca3740e495548b60183e11c7958bad7
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2021-12-02 13:53:16 -0500

    Fixed formatting

--- ConfigMgr/DCM/PowerShell v2 Removal/Detection.ps1
+++ ConfigMgr/DCM/PowerShell v2 Removal/Detection.ps1
@@ -5,7 +5,6 @@ try {
         $Compliant = $false
     }
 }
-
 catch {
     $LastError = $Error | Select-Object -First 1 -ExpandProperty Exception | Select-Object -ExpandProperty Message
     Write-Warning -Message $LastError
