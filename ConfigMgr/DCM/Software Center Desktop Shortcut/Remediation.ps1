fa89a25a9c1ee47dfd2509b5ee1d49b4ce10e06b
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2021-12-02 13:51:28 -0500

    Fixed formatting

--- ConfigMgr/DCM/Software Center Desktop Shortcut/Remediation.ps1
+++ ConfigMgr/DCM/Software Center Desktop Shortcut/Remediation.ps1
@@ -1,7 +1,7 @@
 ﻿$ShortcutPath = "$env:Public\Desktop\Software Center.lnk"
 $Shell = New-Object -COM WScript.Shell
 $Shortcut = $Shell.CreateShortcut($ShortcutPath)
-$Shortcut.TargetPath = "softwarecenter:"
-$Shortcut.Description = "ConfigMgr Software Center"
+$Shortcut.TargetPath = 'softwarecenter:'
+$Shortcut.Description = 'ConfigMgr Software Center'
 $Shortcut.IconLocation = "$env:SystemRoot\CCM\scclient.exe,0"
 $Shortcut.Save()
