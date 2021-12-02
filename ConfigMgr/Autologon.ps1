8f96b3a38d76027d41074484f841a03e764d2a1a
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2021-12-02 13:50:14 -0500

    Fixed formatting, renamed script

--- ConfigMgr/Autologon.ps1
+++
@@ -1,9 +0,0 @@
-$TSEnv = New-Object -COMObject Microsoft.SMS.TSEnvironment
-
-$AutologonUsername = $TSEnv.Value("AutologonUsername")
-$AutologonDomain   = $TSEnv.Value("AutologonDomain")
-$AutologonPassword = $TSEnv.Value("AutologonPassword")
-
-$ScriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
-
-Start-Process -FilePath "$($ScriptDirectory)\Autologon64.exe" -ArgumentList "/AcceptEula $AutologonUsername $AutologonDomain $AutologonPassword"
---
+++ ConfigMgr/Enable-Autologon.ps1
@@ -0,0 +1,9 @@
+$TSEnv = New-Object -ComObject Microsoft.SMS.TSEnvironment
+
+$AutologonUsername = $TSEnv.Value('AutologonUsername')
+$AutologonDomain = $TSEnv.Value('AutologonDomain')
+$AutologonPassword = $TSEnv.Value('AutologonPassword')
+
+$ScriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+Start-Process -FilePath "$($ScriptDirectory)\Autologon64.exe" -ArgumentList "/AcceptEula $AutologonUsername $AutologonDomain $AutologonPassword"
