b5f6abc18096c81bba9e807f607b8cab98528ce6
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2021-12-10 07:52:15 -0500

    Added PR for PowerShell v2 Removal

---
+++ Intune/PR/PowerShell v2 Removal/Detection.ps1
@@ -0,0 +1,21 @@
+﻿try {
+    $Compliant = $true
+
+    if ((((Get-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2).State) -match 'Enabled') -or (((Get-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root).State) -match 'Enabled')) {
+        $Compliant = $false
+    }
+
+    if ($Compliant -eq $true) {
+        Write-Output 'PowerShell v2 is disabled'
+        exit 0
+    }
+    else {
+        Write-Output 'PowerShell v2 is enabled'
+        exit 1
+    }
+}
+catch {
+    $LastError = $Error | Select-Object -First 1 -ExpandProperty Exception | Select-Object -ExpandProperty Message
+    Write-Error -Message $LastError
+    exit 1
+}
---
+++ Intune/PR/PowerShell v2 Removal/Remediation.ps1
@@ -0,0 +1,9 @@
+﻿try {
+    Start-Process -FilePath 'dism.exe' -ArgumentList '/Online', '/Disable-Feature', '/FeatureName:MicrosoftWindowsPowerShellV2', '/FeatureName:MicrosoftWindowsPowerShellV2Root'
+    exit 0
+}
+catch {
+    $LastError = $Error | Select-Object -First 1 -ExpandProperty Exception | Select-Object -ExpandProperty Message
+    Write-Warning -Message $LastError
+    exit 1
+}
