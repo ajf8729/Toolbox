d24677b5c50367adfc72ea7ba6048053e68be2ae
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2022-01-06 17:55:02 -0500

    Added PR scripts for Company Portal Desktop Shortcut

---
+++ Intune/PR/Company Portal Desktop Shortcut/Detection.ps1
@@ -0,0 +1,12 @@
+﻿$SCShortcutPath = "$env:Public\Desktop\Software Center.lnk"
+$CPShortcutPath = "$env:Public\Desktop\Company Portal.url"
+
+if (Test-Path -Path $SCShortcutPath) {
+	Write-Output 'Software Center desktop shortcut exists'
+    exit 1
+}
+
+if ( -not (Test-Path -Path $CPShortcutPath) ) {
+	Write-Output 'Company Portal desktop shortcut does not exist'
+    exit 1
+}
---
+++ Intune/PR/Company Portal Desktop Shortcut/Remediation.ps1
@@ -0,0 +1,12 @@
+﻿$SCShortcutPath = "$env:Public\Desktop\Software Center.lnk"
+
+if (Test-Path -Path $SCShortcutPath) {
+    Remove-Item -Path $SCShortcutPath -Force
+}
+
+$ShortcutPath = "$env:Public\Desktop\Company Portal.url"
+$Shell = New-Object -COM WScript.Shell
+$Shortcut = $Shell.CreateShortcut($ShortcutPath)
+$Shortcut.TargetPath = 'CompanyPortal:'
+$Shortcut.Description = 'Company Portal'
+$Shortcut.Save()
