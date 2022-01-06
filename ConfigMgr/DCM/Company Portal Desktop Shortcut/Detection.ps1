ce9f17834ef86a5fcb9beff87ac5b04d93804c56
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2022-01-06 13:18:55 -0500

    Added DCM scripts for Company Portal Desktop Shortcut

---
+++ ConfigMgr/DCM/Company Portal Desktop Shortcut/Detection.ps1
@@ -0,0 +1,9 @@
+﻿$SCShortcutPath = "$env:Public\Desktop\Software Center.lnk"
+$CPShortcutPath = "$env:Public\Desktop\Company Portal.url"
+
+if ( -not (Test-Path -Path $SCShortcutPath) -and (Test-Path -Path $CPShortcutPath) ) {
+	return $true
+}
+else {
+	return $false
+}
