d4a14bb7be0bbd8a0ca6d9bc5b1f0b7af9cb8da4
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2022-06-28 15:37:08 -0400

    Updated Install-LatestSiteUpdate.ps1

--- ConfigMgr/Install-LatestSiteUpdate.ps1
+++ ConfigMgr/Install-LatestSiteUpdate.ps1
@@ -1,18 +1,14 @@
-[CmdletBinding()]
-Param()
-
-if ( -not (Test-Path -Path "$env:SMS_ADMIN_UI_PATH\..\ConfigurationManager.psd1") ) {
-    Write-Error -Message 'ConfigurationManager module does not exist.'
-    exit
-}
-
 # Import the ConfigurationManager module
-Import-Module -FullyQualifiedName "$env:SMS_ADMIN_UI_PATH\..\ConfigurationManager.psd1"
+Import-Module -Name ConfigurationManager
+
 # Get the site code
 $SiteCode = (Get-CimInstance -Namespace ROOT/SMS -ClassName SMS_ProviderLocation).SiteCode
+
 # Change to the site psdrive
 Set-Location -Path "$($SiteCode):"
+
 # Find the latest update
 $LatestSiteUpdate = (Get-CMSiteUpdate -Fast | Sort-Object -Descending -Property LastUpdateTime | Select-Object -First 1).Name
+
 # Install the update
 Install-CMSiteUpdate -Name $LatestSiteUpdate -Confirm:$false -Force
