e92b05ba4d54e4e2497c3badbc24f8816b172cfd
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2022-07-22 12:15:49 -0400

    Updated cache mgmt scripts

--- ConfigMgr/Cache Management/Clear-CMCache.ps1
+++ ConfigMgr/Cache Management/Clear-CMCache.ps1
@@ -1,8 +1,7 @@
-$CMObject = New-Object -ComObject "UIResource.UIResourceMgr"
+$CMObject = New-Object -ComObject 'UIResource.UIResourceMgr'
 $CMCacheObjects = $CMObject.GetCacheInfo()
 $CMCacheElements = $CMCacheObjects.GetCacheElements()
 
-foreach ($CacheElement in $CMCacheElements)
-{
-    $CMCacheObjects.DeleteCacheElementEx($CacheElement.CacheElementID)
+foreach ($CacheElement in $CMCacheElements) {
+    $CMCacheObjects.DeleteCacheElement($CacheElement.CacheElementID)
 }
