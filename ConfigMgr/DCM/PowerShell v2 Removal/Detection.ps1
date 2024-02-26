5c0149c7685e3db8ef0be78b465982fdc847e6ee
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2024-02-26 09:51:17 -0500

    Rewrote scripts to account for Workstation vs Server OS

--- ConfigMgr/DCM/PowerShell v2 Removal/Detection.ps1
+++ ConfigMgr/DCM/PowerShell v2 Removal/Detection.ps1
@@ -1,6 +1,20 @@
-﻿if (((Get-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root).State -ne 'Disabled') -or ((Get-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2).State -ne 'Disabled')) {
-    return $false
+﻿$Compliant = $true
+$ProductType = (Get-CimInstance -ClassName Win32_OperatingSystem).ProductType
+
+switch ($ProductType) {
+    # Workstation
+    1 {
+        if (((Get-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root).State -ne 'Disabled') -or ((Get-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2).State -ne 'Disabled')) {
+            $Compliant = $false
+        }
+    }
+    # Domain Controller / Server
+    {$_ -in 2, 3} {
+        # Wildcard added to handle if value returned is 'DisabledWithPayloadRemoved'
+        if ((Get-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2).State -notlike 'Disabled*') {
+            $Compliant = $false
+        }
+    }
 }
-else {
-    return $true
-}
+
+return $Compliant
--- ConfigMgr/DCM/PowerShell v2 Removal/Remediation.ps1
+++ ConfigMgr/DCM/PowerShell v2 Removal/Remediation.ps1
@@ -1,1 +1,12 @@
-﻿Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root
+﻿$ProductType = (Get-CimInstance -ClassName Win32_OperatingSystem).ProductType
+
+switch ($ProductType) {
+    # Workstation
+    1 {
+        Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root
+    }
+    # Domain Controller / Server
+    {$_ -in 2, 3} {
+        Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2
+    }
+}
