f014b6a85228a7df7c777f6a48f496bb921dbda7
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2024-03-27 13:09:41 -0400

    Updated to use Get-ItemPropertyValue

--- ConfigMgr/DCM/.NET Strong Crypto/Detection.ps1
+++ ConfigMgr/DCM/.NET Strong Crypto/Detection.ps1
@@ -1,12 +1,12 @@
 $Compliant = $true
 
-if (((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v2.0.50727\' -Name 'SchUseStrongCrypto').SchUseStrongCrypto) -ne 1) {$Compliant = $false}
-if (((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v2.0.50727\' -Name 'SystemDefaultTlsVersions').SystemDefaultTlsVersions) -ne 1) {$Compliant = $false}
-if (((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319\' -Name 'SchUseStrongCrypto').SchUseStrongCrypto) -ne 1) {$Compliant = $false}
-if (((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319\' -Name 'SystemDefaultTlsVersions').SystemDefaultTlsVersions) -ne 1) {$Compliant = $false}
-if (((Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v2.0.50727\' -Name 'SchUseStrongCrypto').SchUseStrongCrypto) -ne 1) {$Compliant = $false}
-if (((Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v2.0.50727\' -Name 'SystemDefaultTlsVersions').SystemDefaultTlsVersions) -ne 1) {$Compliant = $false}
-if (((Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v4.0.30319\' -Name 'SchUseStrongCrypto').SchUseStrongCrypto) -ne 1) {$Compliant = $false}
-if (((Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v4.0.30319\' -Name 'SystemDefaultTlsVersions').SystemDefaultTlsVersions) -ne 1) {$Compliant = $false}
+if ((Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v2.0.50727\' -Name 'SchUseStrongCrypto') -ne 1) {$Compliant = $false}
+if ((Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v2.0.50727\' -Name 'SystemDefaultTlsVersions') -ne 1) {$Compliant = $false}
+if ((Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319\' -Name 'SchUseStrongCrypto') -ne 1) {$Compliant = $false}
+if ((Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319\' -Name 'SystemDefaultTlsVersions') -ne 1) {$Compliant = $false}
+if ((Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v2.0.50727\' -Name 'SchUseStrongCrypto') -ne 1) {$Compliant = $false}
+if ((Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v2.0.50727\' -Name 'SystemDefaultTlsVersions') -ne 1) {$Compliant = $false}
+if ((Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v4.0.30319\' -Name 'SchUseStrongCrypto') -ne 1) {$Compliant = $false}
+if ((Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v4.0.30319\' -Name 'SystemDefaultTlsVersions') -ne 1) {$Compliant = $false}
 
 return $Compliant
