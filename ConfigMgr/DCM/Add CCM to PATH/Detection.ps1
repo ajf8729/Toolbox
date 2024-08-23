3e4e302ec29024cc51b257f097298abfadc25c9e
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2024-08-23 13:56:41 -0400

    Updated scripts to use proper way to get current PATH value

--- ConfigMgr/DCM/Add CCM to PATH/Detection.ps1
+++ ConfigMgr/DCM/Add CCM to PATH/Detection.ps1
@@ -1,6 +1,6 @@
 $Compliant = $False
 $CCMPath = 'C:\Windows\CCM'
-$Paths = [System.Environment]::GetEnvironmentVariable('PATH') -split ';'
+$Paths = (Get-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment').GetValue('PATH', $null, 'DoNotExpandEnvironmentNames') -split ';'
 foreach ($Path in $Paths) {
     if ($Path -eq $CCMPath) {
         $Compliant = $True
--- ConfigMgr/DCM/Add CCM to PATH/Remediation.ps1
+++ ConfigMgr/DCM/Add CCM to PATH/Remediation.ps1
@@ -1,4 +1,4 @@
 $CCMPath = 'C:\Windows\CCM'
-$Paths = [System.Environment]::GetEnvironmentVariable('PATH') -split ';'
+$Paths = (Get-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment').GetValue('PATH', $null, 'DoNotExpandEnvironmentNames') -split ';'
 $NewPath = ($Paths + $CCMPath) -join ';'
 [System.Environment]::SetEnvironmentVariable('PATH', $NewPath, [System.EnvironmentVariableTarget]::Machine)
